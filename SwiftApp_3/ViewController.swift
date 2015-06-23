eadaf7d9b27a4a4628efb3a2e9a129f0f2454f15
liangxm <liangxm_tom@163.com>
2015-06-23 15:38:12 +0800

    first commit for checkin to github

--- SwiftApp_3/ViewController.swift
+++ SwiftApp_3/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    var operand1: String = ""
+    var operand2: String = ""
+    var operate: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didClicked(sender: UIButton) {
+        let value = sender.currentTitle
+        
+        if value == "C" {
+            clear()
+            return
+        }
+        else if value == "+" || value == "-" || value == "*" || value == "/" {
+            operate = value!
+            return
+        }
+        else if value == "=" {
+            var result = 0
+            switch operate {
+                case "+":
+                    result = operand1.toInt()! + operand2.toInt()!
+                case "-":
+                    result = operand1.toInt()! + operand2.toInt()!
+                case "*":
+                    result = operand1.toInt()! + operand2.toInt()!
+                case "/":
+                    result = operand1.toInt()! + operand2.toInt()!
+                default:
+                    result = 0
+            }
+            resultLabel.text = "\(result)"
+            return
+        }
+        
+        if operate == "" {
+            operand1 = operand1 + value!
+            resultLabel.text = operand1
+        }
+        else {
+            operand2 = operand2 + value!
+            resultLabel.text = operand2
+        }
+    }
+    
+    private func clear() {
+        operand1 = ""
+        operand2 = ""
+        operate = ""
+        resultLabel.text = "0"
+    }
 
 }
 
